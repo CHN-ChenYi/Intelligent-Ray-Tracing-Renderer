ecf06de20acfe897a0597ead00c137273c8e7fc0
godwings_thinkpad_desktop <godwings@outlook.com>
2018-08-02 21:36:56 +0800

    Add struct Sphere

--- IRTR.cc
+++ IRTR.cc
@@ -22,7 +22,7 @@ struct Vector {
   Vector operator*(const Vector &rhs) const { // cross
     return Vector(y * rhs.z - z * rhs.y, z * rhs.x - x * rhs.z, x * rhs.y - y * rhs.x);
   }
-  double operator/(const Vector &rhs) { // dot
+  double operator/(const Vector &rhs) const { // dot
     return x * rhs.x + y * rhs.y + z * rhs.z;
   }
   Vector &normalize() {
@@ -32,10 +32,46 @@ struct Vector {
 };
 
 struct Ray {
-  Vector ori, dst;
-  Ray(const Vector &_ori, const Vector &_dst) {
+  Vector ori, dir;
+  Ray(const Vector &_ori, const Vector &_dir) {
     ori = _ori;
-    dst = _dst;
+    dir = _dir;
+  }
+};
+
+enum ReflectionType {
+  Specular, Glass, Diffuse
+};
+struct Sphere {
+  double r;
+  Vector p, e, c;
+  ReflectionType t;
+  static constexpr double eps = 1e-5;
+  Sphere(const double &_radius, const Vector &_position,
+         const Vector &_emission, const Vector &_colour,
+         const ReflectionType &_refl_type) {
+    r = _radius;
+    p = _position;
+    e = _emission;
+    c = _colour;
+    t = _refl_type;
+  }
+  double Intersect(const Ray &ray) { // returns distance, -1 if nohit
+    const Vector o_minus_p = ray.ori - p;
+    const double a = ray.dir / ray.dir;
+    const double b = ray.dir / o_minus_p * 2;
+    const double c = o_minus_p / o_minus_p - r;
+    double delta = b * b - a * c * 4;
+    if (delta < 0)
+      return -1;
+    delta = sqrt(delta);
+    const double t_1 = (- b - delta) / (a * 2);
+    const double t_2 = (- b + delta) / (a * 2);
+    if (t_1 > eps)
+      return t_1;
+    if (t_2 > eps)
+      return t_2;
+    return -1;
   }
 };
 
