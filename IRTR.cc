d4aff43793abda44dfdc4618235936dc5f91fb30
godwings_thinkpad_desktop <godwings@outlook.com>
2018-08-02 21:05:48 +0800

    Add struct Vector

--- IRTR.cc
+++ IRTR.cc
@@ -3,6 +3,34 @@
 #include <cmath>
 #include <cstdio>
 
+struct Vector {
+  double x, y, z;
+  Vector(const double _x = 0., const double _y = 0., const double _z = 0.) {
+    x = _x;
+    y = _y;
+    z = _z;
+  }
+  Vector operator+(const Vector &rhs) const {
+    return Vector(x + rhs.x, y + rhs.y, z + rhs.z);
+  }
+  Vector operator-(const Vector &rhs) const {
+    return Vector(x - rhs.x, y - rhs.y, z - rhs.z);
+  }
+  Vector operator*(const double &rhs) const {
+    return Vector(x * rhs, y * rhs, z * rhs);
+  }
+  Vector operator*(const Vector &rhs) const { // cross
+    return Vector(y * rhs.z - z * rhs.y, z * rhs.x - x * rhs.z, x * rhs.y - y * rhs.x);
+  }
+  double operator/(const Vector &rhs) { // dot
+    return x * rhs.x + y * rhs.y + z * rhs.z;
+  }
+  Vector &normalize() {
+    *this = *this * (1. / sqrt(x * x + y * y + z * z));
+    return *this;
+  }
+};
+
 int main(int argc, char *argv[]) {
 
   return 0;
