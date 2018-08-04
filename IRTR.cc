71b0a4768782390044a7bf04e38da8fe0f40db02
godwings_thinkpad_desktop <godwings@outlook.com>
2018-08-04 22:03:09 +0800

    Finish the first part of Radiance

--- IRTR.cc
+++ IRTR.cc
@@ -3,6 +3,9 @@
 #include <cmath>
 #include <cstdio>
 #include <cstdlib>
+#include <algorithm>
+
+const double inf = 1e31;
 
 struct Vector {
   double x, y, z;
@@ -57,14 +60,14 @@ struct Sphere {
     c = _colour;
     t = _refl_type;
   }
-  double Intersect(const Ray &ray) { // returns distance, -1 if nohit
+  double Intersect(const Ray &ray) const { // returns distance, inf if nohit
     const Vector o_minus_p = ray.ori - p;
     const double a = ray.dir / ray.dir;
     const double b = ray.dir / o_minus_p * 2;
     const double c = o_minus_p / o_minus_p - r;
     double delta = b * b - a * c * 4;
     if (delta < 0)
-      return -1;
+      return inf;
     delta = sqrt(delta);
     const double t_1 = (- b - delta) / (a * 2);
     const double t_2 = (- b + delta) / (a * 2);
@@ -72,7 +75,7 @@ struct Sphere {
       return t_1;
     if (t_2 > eps)
       return t_2;
-    return -1;
+    return inf;
   }
 };
 
@@ -96,8 +99,42 @@ inline int toInt(double x) {
   return int(pow(clamp(x), 1 / 2.2) * 255 + .5);
 }
 
-Vector Radiance(const Ray &r, int depth, unsigned short *Xi) {
-
+int Intersect(const Ray &r, double &t) {
+  int id;
+  t = inf;
+  double tmp;
+  for (int i = int(sizeof(spheres) / sizeof(Sphere)) - 1; i >= 0; i--) {
+    tmp = spheres[i].Intersect(r);
+    if (tmp < t) {
+      id = i;
+      t = tmp;
+    }
+  }
+  if (tmp >= inf)
+    return -1;
+  return id;
+}
+Vector Radiance(const Ray &r, int depth, unsigned short *Xi, double &dis) {
+  double t;
+  int id = Intersect(r, t);
+  if (id < 0) {
+    dis = inf;
+    return Vector();
+  }
+  if (dis < 0)
+    dis = t;
+  const Sphere &obj = spheres[id];  // the hit object
+  Vector col = obj.c;
+  Vector point = r.ori + r.dir * t;  // intersect point
+  Vector n = (point - obj.p).normalize();  // normal
+  Vector o_n = n / r.dir < 0 ? n : n * -1, f = obj.c;  // orinted normal
+  double p = std::max(col.x, std::max(col.y, col.z));  // max reflection
+  if (++depth > 5) {  // Russian Roulette
+    if (erand48(Xi) < p)
+      f = f * (1 / p);
+    else
+      return obj.e;
+  }
 }
 
 int main(int argc, char *argv[]) {
@@ -130,28 +167,32 @@ int main(int argc, char *argv[]) {
       }
     }
   }
+  double dis;
   Vector colour;
   Vector *map = new Vector[w * h];
+  double *depths = new double[w * h];
   Vector cx = Vector(w * lens / h);
   Vector cy = (cx * camera.dir).normalize() * lens;
-  #pragma omp parallel for schedule(dynamic, 1) private(colour)
+  #pragma omp parallel for schedule(dynamic, 1) private(colour, dis)
     for (int y = 0; y < h; y++) {
-    fprintf(stderr, "\rRendering (%d spp) %5.2f%%", samp_num,
-            100. * y / (h - 1));
-    for (unsigned short x = 0, Xi[3] = {0, 0, (unsigned short)(y * y * y)}; x < w; x++) {
-      for (int sy = 0, id = (h - y - 1) * w + x; sy < 2; sy++)
-        for (int sx = 0; sx < 2; sx++, colour = Vector()) {
-          for (int s = 0; s < samp_num; s++) {
-            double r1 = 2 * erand48(Xi);
-            double dx = r1 < 1 ? sqrt(r1) - 1 : 1 - sqrt(2 - r1);
-            double r2 = 2 * erand48(Xi);
-            double dy = r2 < 1 ? sqrt(r2) - 1 : 1 - sqrt(2 - r2);
-            Vector d = cx * (((sx + .5 + dx) / 2 + x) / w - .5) +
-                       cy * (((sy + .5 + dy) / 2 + y) / h - .5) + camera.dir;
-            colour = colour + Radiance(Ray(camera.ori + d * 140, d.normalize()), 0, Xi)
-                     * (1. / samp_num);
+      fprintf(stderr, "\rRendering (%d spp) %5.2f%%", samp_num,
+              100. * y / (h - 1));
+      for (unsigned short x = 0, Xi[3] = {0, 0, (unsigned short)(y * y * y)}; x < w; x++) {
+        for (int sy = 0, id = (h - y - 1) * w + x; sy < 2; sy++) {
+          for (int sx = 0; sx < 2; sx++, colour = Vector()) {
+            for (int s = 0; s < samp_num; s++, dis = -1) {
+              double r1 = 2 * erand48(Xi);
+              double dx = r1 < 1 ? sqrt(r1) - 1 : 1 - sqrt(2 - r1);
+              double r2 = 2 * erand48(Xi);
+              double dy = r2 < 1 ? sqrt(r2) - 1 : 1 - sqrt(2 - r2);
+              Vector d = cx * (((sx + .5 + dx) / 2 + x) / w - .5) +
+                        cy * (((sy + .5 + dy) / 2 + y) / h - .5) + camera.dir;
+              colour = colour + Radiance(Ray(camera.ori + d * 140, d.normalize()), 0, Xi, dis)
+                      * (1. / samp_num);
+              depths[id] += dis * (1. / samp_num);
+            }
+            map[id] = map[id] + Vector(clamp(colour.x), clamp(colour.y), clamp(colour.z)) * .25;
           }
-          map[id] = map[id] + Vector(clamp(colour.x), clamp(colour.y), clamp(colour.z)) * .25;
         }
       }
     }
